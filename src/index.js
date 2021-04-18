39d1f43e3888ab9031e2f78eecefcc2e3ebf0387
Dakota Kronberger <dakota.kronberger@gmail.com>
2021-04-18 14:23:24 -0500

    Added bootstrap, reactstrap

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import './index.css';
 import App from './App';
 import client from './apolloClient';
 import reportWebVitals from './reportWebVitals';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render(
   <React.StrictMode>
